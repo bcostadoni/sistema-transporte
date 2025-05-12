95c9e7e2f36128ea60a8276a87bd37f98a000936
Braian Costadoni <braian.costadoni@tsoftlatam.com>
2025-05-12 13:03:07 -0300

    Primera versión del sitema de transporte - equipo 7

---
+++ src/main/java/unla/isw3/equipo7/sistema_transporte/SistemaTransporteApplication.java
@@ -0,0 +1,13 @@
+package unla.isw3.equipo7.sistema_transporte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaTransporteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaTransporteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/unla/isw3/equipo7/sistema_transporte/SistemaTransporteApplicationTests.java
@@ -0,0 +1,13 @@
+package unla.isw3.equipo7.sistema_transporte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaTransporteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
