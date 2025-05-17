21537b28a3aa4e21608267032433d4af6a044e32
Braian Costadoni <braian.costadoni@tsoftlatam.com>
2025-05-17 17:24:13 -0300

    Se suben cambios del backend de Usuario junto con archivo para insertar datos de prueba

---
+++ src/main/java/unla/isw3/equipo7/sistema_transporte/repository/IUsuarioRepository.java
@@ -0,0 +1,12 @@
+package unla.isw3.equipo7.sistema_transporte.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import unla.isw3.equipo7.sistema_transporte.entity.Usuario;
+import java.util.Optional;
+
+public interface IUsuarioRepository extends JpaRepository<Usuario, Integer> {
+    Optional<Usuario> findByEmail(String email);
+    boolean existsByEmail(String email);
+
+}
